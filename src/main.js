d982971a7626bd6f3d6e6b798f86ac63a56909aa
黃敏睿 <rayhuang@linkchain.tw>
2022-12-29 15:38:06 +0800

    目標

--- src/main.js
+++ src/main.js
@@ -1,36 +1,10 @@
-import DataSourceEPV from './datasource/EPV.js'
-import DataSourceNLSC from './datasource/NLSC.js'
+import DataSource from './adaptor/county.js'
 
 const fragment = document.createDocumentFragment()
 const select = document.createElement(`select`)
 fragment.appendChild(select)
 
-const countyNLSC = await DataSourceNLSC.CountyData()
-const countyNLSCList = Array.from(countyNLSC.querySelectorAll('countyItem'));
-if (countyNLSCList.length) {
-    // #region NLSC
-    const firstOption = document.createElement(`option`)
-    firstOption.value = ''
-    firstOption.text = '請選擇'
-    select.prepend(firstOption)
-    countyNLSCList.map(c => {
-        const option = document.createElement(`option`)
-        option.value = c.querySelector('countycode').textContent
-        option.text = c.querySelector('countyname').textContent
-        return option
-    }).forEach(o => select.appendChild(o))
-    // #endregion NLSC
-} else {
-    // #region EPV
-    // 備援服務
-    const countyEPV = await DataSourceEPV.getCounty()
-    countyEPV.map(c => {
-        const option = document.createElement(`option`)
-        option.value = c.Value
-        option.text = c.Text
-        return option
-    }).forEach(o => select.appendChild(o))
-    // #endregion EPV
-}
+const countyOptions = await DataSource.CountyOptions()
+countyOptions.forEach(o => select.appendChild(o))
 
 document.querySelector(`#ComboContainer`).appendChild(fragment)
