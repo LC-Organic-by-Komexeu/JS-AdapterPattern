3e85a605d58a4e0c9d0935ea205707b7f6682ea9
黃敏睿 <rayhuang@linkchain.tw>
2022-12-30 11:14:14 +0800

    Step 2

--- src/adaptor/county.js
+++ src/adaptor/county.js
@@ -1,13 +1,60 @@
+import DataSourceNLSC from '../datasource/NLSC.js'
+import DataSourceEPV from '../datasource/EPV.js'
+/**
+ * 縣市Adaptor
+ */
 class CountyAdaptor {
+    /**
+     * 取得縣市名稱與代碼
+     * @returns {Array.<HTMLOptionElement>}
+     */
     static async CountyOptions () {
+        let result = []
+
+        result =
+            await this.#ProcessorNLSC() ||
+            await this.#ProcessorEPV()
+        return result
+    }
+
+    static async #ProcessorNLSC () {
         const result = []
 
-        const ele1 = document.createElement('option')
-        ele1.value = 'A'
-        ele1.text = '臺北市'
-        result.push(ele1)
+        const countyNLSC = await DataSourceNLSC.CountyData()
+        const countyNLSCList = Array.from(countyNLSC.querySelectorAll('countyItem'));
+        if (countyNLSCList.length) {
+            const firstOption = document.createElement(`option`)
+            firstOption.value = ''
+            firstOption.text = '請選擇'
+            result.push(firstOption)
 
-        return result
+            countyNLSCList.map(c => {
+                const option = document.createElement(`option`)
+                option.value = c.querySelector('countycode').textContent
+                option.text = c.querySelector('countyname').textContent
+                return option
+            }).forEach(o => result.push(o))
+        }
+
+        if (!result.length) return null
+
+        return result;
+    }
+
+    static async #ProcessorEPV () {
+        const result = []
+
+        const countyEPV = await DataSourceEPV.getCounty()
+        countyEPV.map(c => {
+            const option = document.createElement(`option`)
+            option.value = c.Value
+            option.text = c.Text
+            return option
+        }).forEach(o => result.push(o))
+
+        if (!result.length) return null
+
+        return result;
     }
 }
 
