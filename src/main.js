809c1340aeab535cb4cd6215a374e27b0036b5d3
黃敏睿 <rayhuang@linkchain.tw>
2022-12-30 13:30:30 +0800

    美化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import DataSource from './adaptor/county.js'
 
 const fragment = document.createDocumentFragment()
 const select = document.createElement(`select`)
+select.classList.add('form-select', 'mb-3')
 fragment.appendChild(select)
 
-const countyOptions = await DataSource.CountyOptions(true)
+const countyOptions = await DataSource.CountyOptions()
 countyOptions.forEach(o => select.appendChild(o))
 
-document.querySelector(`#ComboContainer`).appendChild(fragment)
+document.querySelector(`#ComboContainer`).prepend(fragment)
